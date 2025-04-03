863c72bd4014f83c10df5f3e3b418d77c31b4b1b
Digitalis Purpurea <mail@dgtl.is>
2025-04-04 00:25:27 +1030

    Updated to work with Farmer's Delight: Refabricated
    Fixed crash due to new config module for Farmer's Delight
    Added local setting for nourishment display as we can no longer set it for Farmer's Delight
    Adjusted dependencies to get it compiling

--- src/main/java/com/minecraftabnormals/mindful_eating/client/HungerOverlay.java
+++ src/main/java/com/minecraftabnormals/mindful_eating/client/HungerOverlay.java
@@ -2,6 +2,7 @@ package com.minecraftabnormals.mindful_eating.client;
 
 import com.illusivesoulworks.diet.api.type.IDietGroup;
 import com.minecraftabnormals.mindful_eating.compat.FarmersDelightCompat;
+import com.minecraftabnormals.mindful_eating.core.MEConfig;
 import com.minecraftabnormals.mindful_eating.core.MindfulEatingFabric;
 import com.minecraftabnormals.mindful_eating.core.ext.MindfulEatingPlayer;
 import com.nhoryzon.mc.farmersdelight.registry.EffectsRegistry;
@@ -100,8 +101,7 @@ public class HungerOverlay {
 
             if (FabricLoader.getInstance().isModLoaded("farmersdelight")
                     && player.hasEffect(EffectsRegistry.NOURISHMENT.get())
-                    && FarmersDelightCompat.NOURISHED_HUNGER_OVERLAY) {
-                FarmersDelightCompat.setNourishedHungerOverlay(false);
+                    && MEConfig.COMMON.nourishmentOverlay.get()) {
                 texture = GUI_NOURISHMENT_ICONS_LOCATION;
                 icon -= player.hasEffect(MobEffects.HUNGER) ? 45 : 27;
                 background = 0;
